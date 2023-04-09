0b1e1123d3ac38a7c6f25042b6001503af1d735b
hemanth07ds <dshemanth57@gmail.com>
2023-04-09 16:40:03 +0530

    First Commit

---
+++ src/Project/lockedme/main.java
@@ -0,0 +1,16 @@
+package Project.lockedme;
+
+
+
+public class main {
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\Public\\Docs"; 
+	
+	public static void main(String[] args) {
+		menus menu = new menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+
+}
